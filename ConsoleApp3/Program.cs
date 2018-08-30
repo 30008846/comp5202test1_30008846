6f094cc546a712de3d56a027227a36f3fd5a2a34
Gurpreet Singh <30008846@student.toiohomai.ac.nz>
2018-08-30 14:58:00 +1200

    msg

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter number");
+            int num1 = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"{num1}*{i}={num1 * i}");
+            }
+            Console.ReadLine();
+
+            int j = 1;
+            while (j <= 10)
+            {
+                Console.WriteLine($"{num1}*{j}={num1 * j}");
+                j++;
+            }
+
+            int k = 1;
+            do
+            {
+                Console.WriteLine($"{num1}*{k}={num1 * k}");
+                j++;
+            } while (j <= 10);
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            t num1 = 0;
+            Console.WriteLine("enter a number to find odd or even");
+            num1 = int.Parse(Console.ReadLine());
+            if (num1 % 2 == 0)
+            {
+                Console.WriteLine("your name is even");
+            }
+            else
+            {
+                Console.WriteLine("your number is odd");
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
